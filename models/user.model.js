aa1f5b61919195036f869830c3509329833f9dd8
wenodh <vinodkumara3@gmail.com>
2021-11-01 15:53:16 +0530

    user schema added

--- models/user.model.js
+++ models/user.model.js
@@ -1,24 +1,48 @@
 const mongoose = require('mongoose');
-const User = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
+// let validateEmail = function (email) {
+//     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//     return re.test(email);
+// };
+const User = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            unique: true,
+            required: 'Email address is required',
+            // validate: [validateEmail, 'Please fill a valid email address'],
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                'Please fill a valid email address',
+            ],
+        },
+        phone: {},
+        password: {
+            type: String,
+            minlength: 8,
+            maxlength: 16,
+            trim: true,
+            required: true,
+        },
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user',
+        },
+        todoList: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Todo',
+            },
+        ],
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user',
-    },
-});
+    { timestamps: true }
+);
 User.plugin(require('mongoose-paginate-v2'));
 module.exports = mongoose.model('User', User);
