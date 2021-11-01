7272f15bad19f266d42a55720e06de1d47e59019
wenodh <vinodkumara3@gmail.com>
2021-11-01 14:06:50 +0530

    user schema added

---
+++ models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const User = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+});
+User.plugin(require('mongoose-paginate-v2'));
+module.exports = mongoose.model('User', User);
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+mongoose.Promise = global.Promise;
+const URL = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@sandbox.wrvbj.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`;
+mongoose
+    .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('connected to mongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
